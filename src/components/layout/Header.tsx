2abe6788708ef014eee08200c9b2bb7d29a923de
code-yy <jiyeyouren523@gmail.com>
2022-07-24 00:07:56 +0900

    ✨ ヘッダーと一言コメント部分を作成

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,41 @@
 import { FC } from "react";
+import Image from "next/image";
+import { Link as Scroll } from "react-scroll";
+
+const NavLink = [
+  {
+    text: "Vision",
+  },
+  {
+    text: "Problem",
+  },
+  {
+    text: "Service",
+  },
+  {
+    text: "Contact",
+  },
+  {
+    text: "Next",
+  },
+];
 
 export const Header: FC = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex items-center justify-between bg-blue-300 p-4">
+      <Image src="/vercel.svg" alt="/vercel.svg" width={100} height={50} />
+      <nav className="flex items-center justify-between">
+        {NavLink.map((item, index) => (
+          <Scroll
+            key={index}
+            to={item.text}
+            smooth={true}
+            className="mr-12 font-bold hover:cursor-pointer"
+          >
+            {item.text}
+          </Scroll>
+        ))}
+      </nav>
+    </div>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
+import { Layout } from "@/components/layout/Layout";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex justify-start text-end text-lg text-red-400">
-      Hello World
-    </div>
+    <Layout>
+      <div className="bg-blue-300">
+        <div className="mx-auto flex h-[450px] max-w-6xl items-center justify-center px-5 text-9xl">
+          一言タイトル
+        </div>
+      </div>
+    </Layout>
   );
 };
 
