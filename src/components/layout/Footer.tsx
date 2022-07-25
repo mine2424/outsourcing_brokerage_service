c365cc1233b11422b1dd540c3824b253bd036a60
code-yy <jiyeyouren523@gmail.com>
2022-07-26 02:45:17 +0900

    ✨ Footer部分の作成

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,32 @@
 import { FC } from "react";
 
+type FooterLinkListType = {
+  text: string;
+  href: string;
+};
+
+const FooterLinkList: FooterLinkListType[] = [
+  {
+    text: "運営会社",
+    href: "#",
+  },
+  {
+    text: "個人情報利用規約",
+    href: "#",
+  },
+];
+
 export const Footer: FC = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-6 flex flex-col items-center justify-between bg-blue-300 py-16 font-medium">
+      <div>Copyright 2022 西峰, Inc. All Rights Reserved.</div>
+      <ul className="mt-4 flex items-center justify-between gap-5">
+        {FooterLinkList.map((item: FooterLinkListType, index: number) => (
+          <a key={index} href={item.href}>
+            <li>{item.text}</li>
+          </a>
+        ))}
+      </ul>
+    </div>
+  );
 };
