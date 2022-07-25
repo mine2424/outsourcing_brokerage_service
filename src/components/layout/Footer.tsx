8de896abded36ae00dbdb6a41357290dee51bff8
code-yy <jiyeyouren523@gmail.com>
2022-07-26 02:47:24 +0900

    💡 コメントの追加！

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,6 +6,7 @@ type FooterLinkListType = {
 };
 
 const FooterLinkList: FooterLinkListType[] = [
+  // NOTE: 文言と外部リンクは後で変更する
   {
     text: "運営会社",
     href: "#",
@@ -19,6 +20,7 @@ const FooterLinkList: FooterLinkListType[] = [
 export const Footer: FC = () => {
   return (
     <div className="mt-6 flex flex-col items-center justify-between bg-blue-300 py-16 font-medium">
+      {/* NOTE: 後で変更する */}
       <div>Copyright 2022 西峰, Inc. All Rights Reserved.</div>
       <ul className="mt-4 flex items-center justify-between gap-5">
         {FooterLinkList.map((item: FooterLinkListType, index: number) => (
